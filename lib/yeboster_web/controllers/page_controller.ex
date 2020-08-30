4d9a33c9bf296b02ebcbdc7367de439b1004796e
Yeboster <yeboster@gmail.com>
2020-08-30 18:25:37 +0200

    Added routes and pages.

--- lib/yeboster_web/controllers/page_controller.ex
+++ lib/yeboster_web/controllers/page_controller.ex
@@ -4,4 +4,16 @@ defmodule YebosterWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  def about(conn, _params) do
+    render(conn, "about.html")
+  end
+
+  def inspiration(conn, _params) do
+    render(conn, "inspiration.html")
+  end
+
+  def contact(conn, _params) do
+    render(conn, "contact.html")
+  end
 end
