62e3d2ee65d8c7bfc0dee587d42fb66ca9232d56
Yeboster <yeboster@gmail.com>
2021-04-03 18:43:13 +0200

    Add changeset error helper.

--- lib/yeboster/repo.ex
+++ lib/yeboster/repo.ex
@@ -2,4 +2,13 @@ defmodule Yeboster.Repo do
   use Ecto.Repo,
     otp_app: :yeboster,
     adapter: Ecto.Adapters.Postgres
+
+  def changeset_errors_to_string(list) when is_list(list) do
+    Enum.map(list, &changeset_error_to_string/1)
+  end
+
+  def changeset_error_to_string({field, {error, details}})
+       when is_atom(field) and is_bitstring(error) and is_list(details) do
+    "Field '#{field}' #{error}"
+  end
 end
