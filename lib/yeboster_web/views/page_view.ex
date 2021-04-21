558dbb352c7d7ea926def0a647f0312d7d4a8945
Yeboster <yeboster@gmail.com>
2021-04-21 21:39:02 +0200

    Improve about page.

--- lib/yeboster_web/views/page_view.ex
+++ lib/yeboster_web/views/page_view.ex
@@ -1,3 +1,14 @@
 defmodule YebosterWeb.PageView do
   use YebosterWeb, :view
+
+  def my_age do
+    {:ok, birthdate} = Date.new(1999, 06, 29)
+    today = Date.utc_today()
+
+    Date.diff(
+      today,
+      birthdate
+    )
+    |> Integer.floor_div(365)
+  end
 end
