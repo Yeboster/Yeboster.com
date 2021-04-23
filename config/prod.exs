c8d053acd71ee951d6ab6677a2ae5b19aa588d52
Yeboster <yeboster@gmail.com>
2021-04-23 20:06:29 +0200

    Add prod config.

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,9 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :yeboster, YebosterWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "yeboster.me", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
+  server: true
 
 # Do not print debug messages in production
 config :logger, level: :info
--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -23,19 +23,28 @@ secret_key_base =
     You can generate one by calling: mix phx.gen.secret
     """
 
+live_view_signing_salt =
+  System.fetch_env("LIVE_VIEW_SIGNING_SALT") ||
+    raise """
+    environment variable LIVE_VIEW_SIGNING_SALT is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
+app_data_path =
+  System.fetch_env("APP_DATA") || "/app/data"
+
 config :yeboster, YebosterWeb.Endpoint,
   http: [
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
   ],
+  live_view: [
+    signing_salt: live_view_signing_salt
+  ],
   secret_key_base: secret_key_base
+  url: [
+    host: "yeboster.me"
+    port: 443
+  ],
+  app_data_path: app_data_path
 
-# ## Using releases (Elixir v1.9+)
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start each relevant endpoint:
-#
-#     config :yeboster, YebosterWeb.Endpoint, server: true
-#
-# Then you can assemble a release by calling `mix release`.
-# See `mix help release` for more information.
