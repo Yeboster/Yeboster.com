2424bce7023a5b016c6a2d8eafe81bc6072e0f2d
Yeboster <yeboster@gmail.com>
2021-04-24 16:56:10 +0200

    Update release config.

---
+++ config/releases.exs
@@ -0,0 +1,47 @@
+import Config
+
+database_url =
+  System.get_env("DATABASE_URL") ||
+    raise """
+    environment variable DATABASE_URL is missing.
+    For example: ecto://USER:PASS@HOST/DATABASE
+    """
+
+config :yeboster, Yeboster.Repo,
+  # ssl: true,
+  url: database_url,
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  server: true
+
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
+live_view_signing_salt =
+  System.fetch_env("LIVE_VIEW_SIGNING_SALT") ||
+    raise """
+    environment variable LIVE_VIEW_SIGNING_SALT is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
+app_data_path =
+  System.fetch_env("APP_DATA") || "/app/data"
+
+
+config :yeboster, YebosterWeb.Endpoint,
+  http: [
+    port: String.to_integer(System.get_env("PORT") || "4000"),
+    transport_options: [socket_opts: [:inet6]]
+  ],
+  live_view: [
+    signing_salt: live_view_signing_salt
+  ],
+  secret_key_base: secret_key_base,
+  url: [
+    host: "yeboster.me",
+    port: 80
+  ],
+  app_data_path: app_data_path
--- lib/yeboster/release.ex
+++ lib/yeboster/release.ex
@@ -3,12 +3,23 @@ defmodule Yeboster.Release do
   Manage release app cycle (Migration, seeds)
   """
 
+  require Logger
+
   alias Yeboster.Setting.Query, as: Settings
   alias Yeboster.Knowledge.FunFact.Importer
 
   @app :yeboster
 
+  def create_db_if_needed do
+    ensure_started()
+
+    create_db()
+  end
+
+  # TODO: Find a way to start app to use Repo
   def seed_if_needed do
+    ensure_started()
+
     with settings <- Settings.get_settings() do
       unless settings[:seeded] do
         seed()
@@ -34,11 +45,33 @@ defmodule Yeboster.Release do
 
   defp repos do
     Application.load(@app)
+
     Application.fetch_env!(@app, :ecto_repos)
   end
 
   defp ensure_started do
     Application.ensure_all_started(:ssl)
+  end
+
+  defp create_db do
+    ensure_started()
+
+    # Taken from Mix.Tasks.Ecto.Create
+    Enum.each(repos(), fn repo ->
+      case repo.__adapter__.storage_up(repo.config) do
+        :ok ->
+          Logger.info("The database for #{inspect(repo)} has been created")
+
+        {:error, :already_up} ->
+          Logger.info("The database for #{inspect(repo)} has already been created")
+
+        {:error, term} when is_binary(term) ->
+          Logger.error("The database for #{inspect(repo)} couldn't be created: #{term}")
+
+        {:error, term} ->
+          Logger.error("The database for #{inspect(repo)} couldn't be created: #{inspect(term)}")
+      end
+    end)
   end
 
   defp seed do
