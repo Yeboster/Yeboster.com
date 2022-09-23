04bac016ae900afae21da4a4e995aae1722bae76
Yeboster <marco@yeboster.com>
2022-09-23 19:21:18 +0200

    Show active path to navbar

--- lib/yeboster_web/views/layout_view.ex
+++ lib/yeboster_web/views/layout_view.ex
@@ -1,3 +1,18 @@
 defmodule YebosterWeb.LayoutView do
   use YebosterWeb, :view
+
+  defp navbar_link(conn, text, [to: route, class: classes]) do
+    current_path = conn.request_path
+    all_classes = if current_path == route do
+      classes <> " nav-active"
+    else
+      classes <> " nav-inactive"
+    end
+
+    link(
+      text,
+      to: route,
+      class: all_classes
+    )
+  end
 end
